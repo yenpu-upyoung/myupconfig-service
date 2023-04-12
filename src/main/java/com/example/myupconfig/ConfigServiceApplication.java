3d57ff8ac56b04811969d0f960555d736aa10d68
YenPu Chen <yenpu.upyoung@gmail.com>
2023-04-12 14:08:43 +0800

    Add log

--- src/main/java/com/example/myupconfig/ConfigServiceApplication.java
+++ src/main/java/com/example/myupconfig/ConfigServiceApplication.java
@@ -1,14 +1,18 @@
 package com.example.myupconfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+=
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigServiceApplication.class);
     public static void main(String[] args) {
+        LOGGER.info("start app");
         SpringApplication.run(ConfigServiceApplication.class, args);
     }
 }
